fcfa1a9361283242ffa08be2d878c128bb9b01b5
Ivan Darmawan <32323720+ivan471@users.noreply.github.com>
2018-02-20 13:53:07 +0800

    Register dan login

--- app/src/main/java/com/example/user/rumahbuku/RegisterActivity.java
+++ app/src/main/java/com/example/user/rumahbuku/RegisterActivity.java
@@ -9,13 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
     User user;
-    EditText nama,nohp,pass,lib;
+    EditText etnama,nohp,pass,lib;
     CardView register;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference userRef = database.getReference("users");
@@ -23,7 +25,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        nama=(EditText)findViewById(R.id.etnama);
+        etnama=(EditText)findViewById(R.id.etnama);
         lib=(EditText)findViewById(R.id.etnamalib);
         nohp=(EditText)findViewById(R.id.etnomor);
         pass=(EditText)findViewById(R.id.etpass);
@@ -31,8 +33,10 @@ public class RegisterActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(nama.getText().toString().length()==0){
-                    nama.setError("Kosong!");
+                final String nomor = nohp.getText().toString();
+                final String nama = etnama.getText().toString();
+                if(etnama.getText().toString().length()==0){
+                    etnama.setError("Kosong!");
                 }else if(lib.getText().toString().length()==0){
                     lib.setError("Kosong!");
                 }else if(nohp.getText().toString().length()==0){
@@ -44,17 +48,35 @@ public class RegisterActivity extends AppCompatActivity {
                 }else if(pass.getText().toString().length()<=6){
                     pass.setError("Minimal 6 digit!");
                 }else {
-                    user = new User();
-                    user.setNama(nama.getText().toString());
-                    user.setNamalib(lib.getText().toString());
-                    user.setTelepon(nohp.getText().toString());
-                    user.setPassword(pass.getText().toString());
-                    user.register();
-                    Toast.makeText(getApplicationContext(), "Register Sukses", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
+                    userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            User user1 = dataSnapshot.getValue(User.class);
+                            if (dataSnapshot.child(nomor).exists()) {
+                                Toast.makeText(getApplicationContext(), "Nomor Hp sudah digunakan", Toast.LENGTH_SHORT).show();
+                            }else if (user1.getNama().equals(nama)){
+                                Toast.makeText(getApplicationContext(), "Nama sudah digunakan", Toast.LENGTH_SHORT).show();
+                            }else{
+                            user = new User();
+                            user.setNama(etnama.getText().toString());
+                            user.setNamalib(lib.getText().toString());
+                            user.setTelepon(nohp.getText().toString());
+                            user.setPassword(pass.getText().toString());
+                            user.register();
+                            Toast.makeText(getApplicationContext(), "Register Sukses", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            }
+                        }
 
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+
+
+                    });
                 }
             }
         });
